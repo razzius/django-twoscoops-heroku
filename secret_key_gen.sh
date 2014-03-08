ad9f31f7a9c5454d1485c1841e70d59ad2d17d7f
Razzi <razzi53@gmail.com>
2014-03-08 04:40:47 -0500

    dj-static requirements fix

--- secret_key_gen.sh
+++ secret_key_gen.sh
@@ -6,6 +6,7 @@ echo "Generated secret key {{ secret_key }}"
 echo "Adding secret key to virtual environment..."
 echo "export SECRET_KEY='{{ secret_key }}'" >> $VIRTUAL_ENV/bin/activate
 echo "Restarting virtual environment..."
-source $VIRTUAL_ENV/bin/activate
+deactivate
+. $VIRTUAL_ENV/bin/activate
 echo "Self-destructing..."
 rm secret_key_gen.sh
